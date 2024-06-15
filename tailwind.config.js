c4f20194f39efd7eca8d1bb995281f42bb469c6e
minhtuan007 <damminhtuan2006@gmail.com>
2024-06-15 10:56:39 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   mode: "jit",
-  // ...
+ 
+  theme: {
+    screens: {
+      'mobile': {'max': '739px'},
+      // => @media (max-width: 740px) { ... }
+
+      'PC': '740px',
+      // => @media (min-width: 1024px) { ... }
+    },
+  },
   purge: [
-    // Example content paths...
+
     "./**/*.html",
     "./**/*.{js,jsx,ts,tsx,vue}",
   ],
-  // ...
+
 };
